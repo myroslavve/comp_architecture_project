d9c7f109f19d5a6cc4421d25a0e6f551a181d082
TeksPeks <verstyukm@gmail.com>
2024-03-08 12:53:18 +0200

    implement input also temporary outputting all read characters

--- 1.asm
+++ 1.asm
@@ -2,11 +2,33 @@
 .stack 100h
 
 .data
-    array db 10 dup(0)
+    oneChar db 0
 
 .code
 main proc
-         int 21h    ; Call the DOS interrupt to terminate the program
+    read_next:
+              mov ah, 3Fh
+              mov bx, 0h                ; stdin handle
+              mov cx, 1                 ; 1 byte to read
+              mov dx, offset oneChar    ; read to ds:dx
+              int 21h                   ; cx = number of bytes read
+
+    ; check if end of file (EOF) reached
+              cmp ax, 0                 ; check if EOF reached
+              je  end_read              ; jump to end_read if EOF reached
+
+    ; print the character
+              mov ah, 02h               ; DOS function 2: display character
+              mov dl, oneChar           ; character to print
+              int 21h                   ; Call the DOS interrupt
+            
+              jmp read_next             ; jump back to read_next to read the next character
+
+    end_read: 
+    ; end of file reached, do something here if needed
+
+
+              int 21h                   ; Call the DOS interrupt to terminate the program
 main endp
 
 end main
