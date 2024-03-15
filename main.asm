6f94e4da5fcb8fddca46178c8fb91c6738014b4e
TeksPeks <verstyukm@gmail.com>
2024-03-15 11:59:12 +0200

    Add `StrLength` function

--- main.asm
+++ main.asm
@@ -7,28 +7,54 @@
 .code
 main proc
     read_next:
-              mov ah, 3Fh
-              mov bx, 0h                ; stdin handle
-              mov cx, 1                 ; 1 byte to read
-              mov dx, offset oneChar    ; read to ds:dx
-              int 21h                   ; cx = number of bytes read
+              mov   ah, 3Fh
+              mov   bx, 0h                ; stdin handle
+              mov   cx, 1                 ; 1 byte to read
+              mov   dx, offset oneChar    ; read to ds:dx
+              int   21h                   ; cx = number of bytes read
 
     ; check if end of file (EOF) reached
-              cmp ax, 0                 ; check if EOF reached
-              je  end_read              ; jump to end_read if EOF reached
+              cmp   ax, 0                 ; check if EOF reached
+              je    end_read              ; jump to end_read if EOF reached
 
     ; print the character
-              mov ah, 02h               ; DOS function 2: display character
-              mov dl, oneChar           ; character to print
-              int 21h                   ; Call the DOS interrupt
+              mov   ah, 02h               ; DOS function 2: display character
+              mov   dl, oneChar           ; character to print
+              int   21h                   ; Call the DOS interrupt
             
-              jmp read_next             ; jump back to read_next to read the next character
+              jmp   read_next             ; jump back to read_next to read the next character
 
     end_read: 
     ; end of file reached, do something here if needed
 
 
-              int 21h                   ; Call the DOS interrupt to terminate the program
+              int   21h                   ; Call the DOS interrupt to terminate the program
 main endp
+
+    ;---------------------------------------------------------------
+    ; StrLength     Count non-null characters in a string
+    ;---------------------------------------------------------------
+    ; Input:
+    ;       di = address of string (s)
+    ; Output:
+    ;       cx = number of non-null characters in s
+    ; Registers:
+    ;       cx
+    ;---------------------------------------------------------------
+StrLength proc
+              push  ax                    ; Save modified registers
+              push  di
+
+              xor   al, al                ; al <- search char (null)
+              mov   cx, 0ffffh            ; cx <- maximum search depth
+              cld                         ; Auto-increment di
+              repnz scasb                 ; Scan for al while [di]<>null & cx<>0
+              not   cx                    ; Ones complement of cx
+              dec   cx                    ;  minus 1 equals string length
+
+              pop   di                    ; Restore registers
+              pop   ax
+              ret                         ; Return to caller
+StrLength endp
 
 end main
